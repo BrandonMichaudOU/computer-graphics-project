5739f5325ffb2b5231ca67549d619f5f483d02da
Brandon Michaud <brandondmichaud@ou.edu>
2023-02-24 10:06:11 -0600

    Copied gradle wrapper from homework02 and rewrote gradle.build for purposes of this project

---
+++ cg-project/src/main/java/Project.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Project {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        Scanner sc = new Scanner(System.in);
+        while (!sc.nextLine().equals("Q")) {
+
+        }
+        sc.close();
+    }
+}
