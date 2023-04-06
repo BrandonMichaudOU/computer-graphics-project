59ff71e2e860aa17d8f92f0df2343d30ee75cf49
Brandon Michaud <Brandon.D.Michaud-1@ou.edu>
2023-04-06 15:56:16 -0500

    Added depths for SearchNodes

--- cg-project/ou-cs-cg/src/main/java/edu/ou/cs/cg/project/SearchNode.java
+++ cg-project/ou-cs-cg/src/main/java/edu/ou/cs/cg/project/SearchNode.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class SearchNode {
     public SearchNode parent;
     public Node node;
+    public int depth;
 
     public SearchNode(Node node) {
         this.parent = null;
