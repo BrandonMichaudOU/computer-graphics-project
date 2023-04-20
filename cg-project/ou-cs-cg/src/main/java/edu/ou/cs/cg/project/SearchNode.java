075faa97fce9c278c685820a1b4dddacae533e1a
trev97 <trevorscogin97@gmail.com>
2023-04-20 15:18:08 -0500

    Added new parameter to searchNode

--- cg-project/ou-cs-cg/src/main/java/edu/ou/cs/cg/project/SearchNode.java
+++ cg-project/ou-cs-cg/src/main/java/edu/ou/cs/cg/project/SearchNode.java
@@ -1,14 +1,22 @@
 package edu.ou.cs.cg.project;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class SearchNode {
     public SearchNode parent;
     public Node node;
     public int depth;
+    public int weight;
 
     public SearchNode(Node node) {
         this.parent = null;
         this.node = node;
+        this.weight=1;
+    }
+
+    public void randomNum(){
+        Random r = new Random();
+        weight= r.nextInt(5)+1;
     }
 }
